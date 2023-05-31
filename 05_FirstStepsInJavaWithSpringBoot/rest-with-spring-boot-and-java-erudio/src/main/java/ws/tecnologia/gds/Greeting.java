5cf49d630bf9c427299fd9a0585583422d714039
Gesiel Diniz da Silva <gesiel.diniz@gmail.com>
2023-05-31 19:35:54 -0300

    First Steps in Java with Spring Boot 3

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/ws/tecnologia/gds/Greeting.java
@@ -0,0 +1,27 @@
+package ws.tecnologia.gds;
+
+public class Greeting {
+	
+	private final long id;
+	private final String content;
+	
+	
+	public Greeting(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+	
+}
